4a1081a295da3c297c9281406d4c5c327ab1b624
creeper_command <creepercommand07@gmail.com>
2020-03-21 14:52:25 +0900

    데이터 셋 불러오고 확인하기

---
+++ pyai01.py
@@ -0,0 +1,8 @@
+import sklearn.datasets
+
+digits = sklearn.datasets.load_digits()
+
+print("데이터의 개수:",len(digits.images))
+print("이미지 데이터: ---------------------------")
+print(digits.images[0])
+print("무슨 데이터:", digits.target[0])
